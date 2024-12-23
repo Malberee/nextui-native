b7246ade0a1d8e26ecc4a848316a65c62215322c
Malberee <82259650+Malberee@users.noreply.github.com>
2024-12-23 21:53:59 +0200

    fix(slider): imports

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,4 +1,3 @@
-import { Checkbox, CheckboxGroup } from '@malberee/nextui-native'
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
 
@@ -6,30 +5,7 @@ import '../global.css'
 
 const App = () => {
   return (
-    <View className="bg-background dark px-4" style={styles.container}>
-      <CheckboxGroup>
-        <Checkbox
-          value="1"
-          classNames={{ wrapper: 'group-[[selected=true]]:bg-white' }}
-        >
-          1
-        </Checkbox>
-        <Checkbox
-          value="2"
-          classNames={{
-            wrapper: 'group-[[selected=true]]:scale-150 transition-transform',
-          }}
-        >
-          2
-        </Checkbox>
-        <Checkbox
-          value="3"
-          classNames={{ wrapper: 'group-[[selected=true]]:bg-white' }}
-        >
-          3
-        </Checkbox>
-      </CheckboxGroup>
-    </View>
+    <View className="bg-background dark px-4" style={styles.container}></View>
   )
 }
 
--- src/components/slider/index.ts
+++ src/components/slider/index.ts
@@ -1,3 +1,16 @@
 import Slider from './slider'
+import Thumb from './slider-thumb'
 
-export { Slider }
+// export types
+export type { SliderProps } from './slider'
+export type {
+  SliderValue,
+  SliderStepMark,
+  SliderRenderThumbProps,
+} from './hooks'
+
+// export hooks
+export { useSlider, useSliderThumb } from './hooks'
+
+// export component
+export { Slider, Thumb }
